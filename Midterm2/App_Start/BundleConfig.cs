827dbe0e53a7fe05d024e069dabf67cb0b13063f
Jean-Luc Desroches <jean18801880@gmail.com>
2015-07-02 15:19:37 -0400

    add theming

--- Midterm2/App_Start/BundleConfig.cs
+++ Midterm2/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Midterm2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/bower_components/bootstrap-material-design/dist/css/material.css"));
         }
     }
 }
